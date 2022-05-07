1b65a6f1b88a0bb081bdbbd601122907f33291b7
Juan David <juandavid1527@gmail.com>
2022-05-07 18:23:11 -0500

    Top is created & functionalities are added to the race class

--- src/main/java/co/com/sofka/game/Race.java
+++ src/main/java/co/com/sofka/game/Race.java
@@ -10,9 +10,7 @@ import static co.com.sofka.util.UnitMeasure.METROS;
 
 public class Race {
     private static Scanner read = new Scanner(System.in);
-
     private static final String NAME_TRACK = "Solaris";
-
     private String id;
     private byte numberPlayers;
     private List<Driver> drivers;
@@ -41,6 +39,39 @@ public class Race {
         track.setName(NAME_TRACK);
         track.setDistance(DIEZ_MIL.getValue());
         track.setUnitMeasure(METROS.getValue());
+
+        lanes = new ArrayList<>();
+
+        for (int i = 0; i < numberPlayers; i++) {
+            System.out.println("Ingrese los datos del conductor: " + (i + 1));
+            Driver driver = new Driver();
+            System.out.println("Identificador del jugador: ");
+            driver.setId(read.nextLine());
+            System.out.println("Apodo: ");
+            driver.setNickname(read.nextLine());
+            System.out.println("Edad: ");
+            driver.setAge(read.nextByte());
+
+            System.out.println("Ingrese los datos del vehiculo: " + (i + 1));
+            Car car = new Car();
+            System.out.println("Identificador del vehiculo: ");
+            car.setId(read.nextLine());
+            System.out.println("Modelo: ");
+            car.setModel(read.nextLine());
+            System.out.println("Marca: ");
+            car.setBrand(read.nextLine());
+            System.out.println("Linea: ");
+            car.setLine(read.nextLine());
+
+            driver.setCar(car);
+            drivers.add(driver);
+
+            Lane lane = new Lane();
+            lane.setId(String.valueOf((i + 1)));
+            lane.setCar(car);
+            lanes.add(lane);
+        }
+        track.setLanes(lanes);
     }
 
 }
---
+++ src/main/java/co/com/sofka/util/Top.java
@@ -0,0 +1,8 @@
+package co.com.sofka.util;
+
+public enum Top {
+    SIN_TOP,
+    TOP_1,
+    TOP_2,
+    TOP_3;
+}
